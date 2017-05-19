30fe6fdf3310579f4ccdbbaa708eef52de607b06
Dhyanandra Singh <NxtLife@Dhyanandras-iMac.local>
2017-05-19 17:23:11 +0530

    Applying changes as in Corner stone management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,17 +20,17 @@ declare let $;
 })
 export class AppComponent implements AfterViewInit{
   public pages = [
-      { title: 'Home', component: HomeComponent, icon: 'assets/icon/home.png', url: 'home' },
-      { title: 'Dashboard', component: DashboardComponent, icon: 'assets/icon/home.png', url: 'dashboard' },
-      { title: 'Complaints', component: ComplaintComponent, icon: 'assets/icon/complaint.png', url: 'complaint' },
-      { title: 'Suggestions', component: SuggestionComponent, icon: 'assets/icon/suggestion.png', url: 'suggestion' },
-      { title: 'Appreciations', component: AppreciationComponent, icon: 'assets/icon/appreciation.png', url: 'appreciation' },
-      { title: 'Messaging', component: MessageComponent, icon: 'assets/icon/message.png', url: 'conversation' },
-      { title: 'Events', component: EventComponent, icon: 'assets/icon/event.png', url: 'planner' },
-      { title: 'Poll', component: PollComponent, icon: 'assets/icon/poll.png', url: 'poll' },
-      { title: 'Survey', component: SurveyComponent, icon: 'assets/icon/survey.png', url: 'survey' },
-      { title: 'Circular', component: CircularComponent , icon: 'assets/icon/homework.png', url: 'circular'},
-      { title: 'Homework', component: HomeworkComponent, icon: 'assets/icon/circular.png', url: 'homework' }
+      { title: 'Home', component: HomeComponent, icon: 'icons/home.png', url: 'home' },
+      { title: 'Dashboard', component: DashboardComponent, icon: 'icons/dashboard.png', url: 'dashboard' },
+      { title: 'Complaints', component: ComplaintComponent, icon: 'icons/complaint.png', url: 'complaint' },
+      { title: 'Suggestions', component: SuggestionComponent, icon: 'icons/suggestion.png', url: 'suggestion' },
+      { title: 'Appreciations', component: AppreciationComponent, icon: 'icons/appreciation.png', url: 'appreciation' },
+      { title: 'Messaging', component: MessageComponent, icon: 'icons/message.png', url: 'conversation' },
+      { title: 'Events', component: EventComponent, icon: 'icons/event.png', url: 'planner' },
+      { title: 'Poll', component: PollComponent, icon: 'icons/poll.png', url: 'poll' },
+      { title: 'Survey', component: SurveyComponent, icon: 'icons/survey.png', url: 'survey' },
+      { title: 'Circular', component: CircularComponent , icon: 'icons/homework.png', url: 'circular'},
+      { title: 'Homework', component: HomeworkComponent, icon: 'icons/circular.png', url: 'homework' }
     ];
   constructor(public auth:AuthService, public log:LoggedInGuard){
     
